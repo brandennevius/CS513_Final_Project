74b2944c956fe5138aa8e72402bfbf2c5c29cab9
Branden Nevius <brandennevius@gmail.com>
2022-07-23 13:20:31 -0400

    updated removeCols() function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from hashlib import new
+from turtle import update
 import pandas as pd
 
 # Get dataset
@@ -9,7 +11,11 @@ def getDataset():
 # Defining main function
 def main():
     print("test")
-    # getDataset()
+    df = getDataset()
+    df = removeCols(df)
+    # print(df.head())
+    # print(getNullColumns())
+
 
 
 # Count the Null columns
@@ -21,25 +27,25 @@ def getNullColumns():
     null_columns=df.columns[df.isnull().any()]
     print(df[null_columns].isnull().sum())
 
-getNullColumns()
-
 # Remove the region 1 and region 2
 """
 Region 1 contains 16% null data 
 Region 2 contains 61% data 
 param col_name : String of the column name we want to delete
 """
-def removeCol(col_name):
-    df = df.drop(col_name, axis=1)
+def removeCols(df):
+    new_df = df.drop('region_1', axis=1)
+    new_df = new_df.drop('region_2', axis=1)
+    new_df = new_df.drop('description', axis=1)
+    new_df = new_df.drop('taster_name', axis=1)
+    new_df = new_df.drop('taster_twitter_handle', axis=1)
+    return new_df
+    
 
   
 # Using the special variable 
 # __name__
 if __name__=="__main__":
     main()
-    removeCol('region_1')
-    removeCol('region_2')
-    removeCol('taster_name')
-    removeCol('taster_twitter_handle')
-    removeCol('description')
+    
     
